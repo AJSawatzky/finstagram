34438df63c990cdcf8d6e9b915f9e9e89e4c2b45
AJSawatzky <andrassawatzky@gmail.com>
2022-07-20 03:18:46 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,17 +1,13 @@
-get '/' do
-  File.read(File.join('app/views', 'index.html'))
-end
-
-def humanized_time_ago(time_ago_in_minutes)
-  if time_ago_in_minutes >= 60
-    "#{time_ago_in_minutes / 60} hours ago"
+def humanized_time_ago(minute_num)
+    if  minute_num  >= 60
+    "#{minute_num / 60} hours ago"
   else
-    "#{time_ago_in_minutes} minutes ago"
+    "#{minute_num} minutes ago"
   end
 end
  
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -24,7 +20,7 @@ get '/' do
     }]
   }
  
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -37,7 +33,7 @@ get '/' do
     }]
   }
  
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -49,14 +45,10 @@ get '/' do
       text: "lunchtime! ;)"
     }]
   }
+
+  [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+  erb(:index) 
+
 end
 
-get '/' do
 
-  # ...
-  # Here you would have all of the code for your posts
-  # ...
-
-  # This is the new addition
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
-end 
